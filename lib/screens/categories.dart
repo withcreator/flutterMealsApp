5f5bb3d303e6a18c19c0bd02a00e611d51b1be2c
creator <th8creator@gmail.com>
2025-02-24 09:29:40 +0300

    first commit

---
+++ lib/screens/categories.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals/models/category.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredmeals =
+        dummyMeals
+            .where((meal) => meal.categories.contains(category.id))
+            .toList();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder:
+            (ctx) => MealsScreen(title: category.title, meals: filteredmeals),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(20),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          ),
+      ],
+    );
+  }
+}
